1e05d236a90fb3590390e9ee367dce6c5623f198
soerengh <soeren.imhof@gigahertz.de>
2020-08-29 13:30:59 +0200

    new polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,11 +7,13 @@ killall -q polybar
 while pgrep -u $USER -x polybar >/dev/null; do sleep 1; done
 
 ## Launch
-polybar system -c ~/.config/polybar/config.ini &
-polybar apps -c ~/.config/polybar/config.ini &
-polybar workspace -c ~/.config/polybar/config.ini &
-polybar wallpaper -c ~/.config/polybar/config.ini &
-polybar light -c ~/.config/polybar/config.ini &
-polybar network -c ~/.config/polybar/config.ini &
-polybar info -c ~/.config/polybar/config.ini &
+# polybar system -c ~/.config/polybar/config.ini &
+# polybar apps -c ~/.config/polybar/config.ini &
+# polybar workspace -c ~/.config/polybar/config.ini &
+# polybar wallpaper -c ~/.config/polybar/config.ini &
+# polybar light -c ~/.config/polybar/config.ini &
+# polybar network -c ~/.config/polybar/config.ini &
+# polybar info -c ~/.config/polybar/config.ini &
 
+polybar static-left -c ~/.config/polybar/config.ini &
+polybar static-right -c ~/.config/polybar/config.ini &
